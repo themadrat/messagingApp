ab4b5eb6bb9ec1a5f4a319f0e0179574fe740700
themadrat <Jaredshaddick@gmail.com>
2022-02-15 11:11:26 -0400

    Made a Change that Allowed for Specifying the  Server Name

--- src/messagingApp/GreetingClient.java
+++ src/messagingApp/GreetingClient.java
@@ -9,33 +9,51 @@ import java.io.*;
 
 public class GreetingClient
 {
+	public String serverName  = "localhost";
+	
+	private int port = 6066;											// Hard-code port for connection
 	public void sendClient(String JsonPacket) {
+		/*
+		 * Method:				sendClient
+		 * 
+		 * Method Parameters:	String JsonPacket
+		 * 
+		 * Method Return:		void
+		 * 
+		 * Synopsis:			This method will be the point of
+		 * 						communication between this client
+		 * 						and the server.
+		 * 
+		 * Modifications:		Date:		Name:			Modification:
+		 * 						<02/08/2022	Stephen Johnson	Original Client Code
+		 * 						02/08/2022 	Jared Shaddick	Created Method to Prevent
+		 * 													the Client from Starting First
+		 * 						02/15/2022	Jared Shaddick	Adapted Code to Allow Specification of
+		 * 													the Server Name
+		 */
 	      //String serverName = args[0];								// Used for command line parameter
 	      //int port = Integer.parseInt(args[1]);						// Used for command line parameter
-	      
-	      String serverName  = "localhost";								// Establish the local machine as the
-	      																// server
-	      int port = 6066;												// Hard-code port for connection
-	      try
-	      {
-	         System.out.println("Connecting to " + serverName
-	                             + " on port " + port);
-	         Socket client = new Socket(serverName, port);
-	         System.out.println("Just connected to "
-	                      + client.getRemoteSocketAddress());
-	         OutputStream outToServer = client.getOutputStream();
-	         DataOutputStream out =
-	                       new DataOutputStream(outToServer);
+			
+		try
+	    {
+			System.out.println("Connecting to " + serverName
+	                            + " on port " + port);
+	        Socket client = new Socket(serverName, port);
+	        System.out.println("Just connected to "
+	                     + client.getRemoteSocketAddress());
+	        OutputStream outToServer = client.getOutputStream();
+	        DataOutputStream out =
+	                      new DataOutputStream(outToServer);
 
-	         out.writeUTF(JsonPacket);
-	         InputStream inFromServer = client.getInputStream();
-	         DataInputStream in =
-	                        new DataInputStream(inFromServer);
-	         System.out.println("Server says " + in.readUTF());
-	         client.close();
-	      }catch(IOException e)
-	      {
+	        out.writeUTF(JsonPacket);
+	        InputStream inFromServer = client.getInputStream();
+	        DataInputStream in =
+                        new DataInputStream(inFromServer);
+	        System.out.println("Server says " + in.readUTF());
+	        client.close();
+	    }catch(IOException e)
+	    {
 	         e.printStackTrace();
-	      }
+	    }
 	}
 }
